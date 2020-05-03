8a8ccf8b7eb11a09ec8a6f9674e8f9626b8685b6
TheDesertedSea <745468443@qq.com>
2020-05-03 09:42:52 +0800

    yes

--- Player/temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,6 +0,0 @@
-
-require('./assets/scripts/Bullet');
-require('./assets/scripts/Game');
-require('./assets/scripts/Joystick');
-require('./assets/scripts/Player');
-require('./assets/scripts/Weapon');
