392b1f2c36873a36c4bc927e9049bd04bdf39f9b
Z-nkk <1074505539@qq.com>
2020-05-22 20:50:28 +0800

    优化代码和一些其他部分，设置窗口居中，Player-UI的scale改为1

--- Player/assets/scripts/Play_Setting.js
+++ Player/assets/scripts/Play_Setting.js
@@ -19,10 +19,8 @@ cc.Class({
                 var node = cc.instantiate(this.Setting_Menu);
                 this.node.addChild(node);
                 this.Setting_Menu.active = true
-                this.node.pauseSystemEvents(true); //暂停当前节点上注册的所有节点系统事件
             }
-            if (data == 'setting'){}
-        },
         // update (dt) {},
+        },
 });
 
--- Player/assets/scripts/baseView.js
+++ Player/assets/scripts/baseView.js
@@ -10,10 +10,6 @@ cc.Class({
 
 
     onLoad () {
-//      var spr = this.node.getChildByName('Start_Button')
-//      cc.log(spr)
-//      var spr = this.node.getComponent()
-//      cc.log(spr)
       var spr = this.node.getChildByname('aaa')
     },
 
@@ -21,20 +17,11 @@ cc.Class({
     },
     
     onClickButton: function(target, data){
-//      cc.log(data)
       if (data == 'START'){
-//        cc.log('myFunction!')
-//        this.Start_Button.spriteFrame = this.mySprite
-//      cc.log(this.myButton)
       this.myStart_Label.enabled = false
       this.myStart_Button.spriteFrame = this.mySprite
       cc.director.loadScene("Play")
       }
-//      if (data == 'setting'){
-//        if(this.Setting_Menu == null){
-//          this.Setting_Menu = cc.instanca
-//        }
-//      }
     },
      onClickSetting:function (target, data) {
       if (data == 'setting'){
@@ -42,8 +29,6 @@ cc.Class({
         this.node.addChild(node);
         this.Setting_Menu.active = true
       }
-      if (data == 'setting'){}
-        
-    },
     // update (dt) {},
+    }
 });
