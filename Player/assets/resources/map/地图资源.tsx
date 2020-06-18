93c11c083c6429fc5e379b1f8dbd118e083b379b
TheDesertedSea <49360881+TheDesertedSea@users.noreply.github.com>
2020-06-18 21:11:49 +0800

    清空

--- Player/assets/resources/map/地图资源.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="地图资源" tilewidth="16" tileheight="16" tilecount="2048" columns="16">
- <image source="地图资源.png" width="256" height="2048"/>
-</tileset>
--- Player/assets/resources/角色1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="复古像素地牢洞穴场景素材-Tiny Platform Que_爱给网_aigei_com(1)" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="复古像素地牢洞穴场景素材-Tiny Platform Que_爱给网_aigei_com(1).png" width="512" height="512"/>
-</tileset>
