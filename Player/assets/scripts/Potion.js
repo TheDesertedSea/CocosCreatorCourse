d0b79c680b10bd4f72ca14f0e71c01f03bfc6896
TheDesertedSea <49360881+TheDesertedSea@users.noreply.github.com>
2020-06-18 12:50:18 +0800

    物品、武器靠近时显示名称

--- Player/assets/scripts/Potion.js
+++ Player/assets/scripts/Potion.js
@@ -13,7 +13,6 @@ cc.Class({
         potionAdd:25,
         time:5,
         type:"DFAULT",
-        
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -38,6 +37,7 @@ cc.Class({
             PlayerScript.damageAddTime=this.time;
             //cc.log(this.player.getChildByName("weapon").getComponent("Weapon").damage);
         }
+        this.node.getChildByName("nameLabel").destroy();
         this.node.destroy();
     },
 
@@ -54,6 +54,10 @@ cc.Class({
             this.player.getComponent("Player").bUseItem=true;
             //cc.log(this.player.getComponent("Player").bUseItem);
             this.playerAround=true;
+
+            //显示物品名称
+            this.node.getChildByName("nameLabel").active=true;
+
         }
         if(this.node.getPosition().sub(this.player.getPosition()).mag()>=50&&this.playerAround)
         {
@@ -61,6 +65,9 @@ cc.Class({
             this.player.getComponent("Player").itemAround=null;
             this.player.getComponent("Player").bUseItem=false;
             this.playerAround=false;
+
+            //关闭物品名称显示
+            this.node.getChildByName("nameLabel").active=false;
         }
     },
 });
